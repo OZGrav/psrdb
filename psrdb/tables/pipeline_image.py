690d5e2c9a12dfb0f08173a8b91a5d314741b1d0
NickSwainston <nickaswainston@gmail.com>
2023-06-21 10:17:14 +0800

    Can now upload PipelineImages

--- psrdb/tables/pipeline_image.py
+++ psrdb/tables/pipeline_image.py
@@ -1,31 +1,11 @@
-from psrdb.graphql_table import GraphQLTable
-from psrdb.graphql_query import graphql_query_factory
+import requests
 from base64 import b64encode
 
-
-def prepare_image(img_fn):
-    """
-    open a file and encode contents in base64 for submission via graphql
-    this ensures that all characters we're sending are acceptable
-
-    input:
-    img_fn: string with file name to the image file (we do not verify the file is really an image)
-
-    returns: b64-encoded string with contents of the file
-    """
-    img_bytes = ""
-    if img_fn and img_fn != '""':
-        with open(img_fn, "rb") as fh:
-            img_bytes = fh.read()
-    # provided file name could point to an empty file
-    if img_bytes:
-        # encode and strip the bytes marker
-        img_bytes = str(b64encode(img_bytes))[2:-1]
-
-    return img_bytes
+from psrdb.graphql_table import GraphQLTable
+from psrdb.graphql_query import graphql_query_factory
 
 
-class Pipelineimages(GraphQLTable):
+class PipelineImage(GraphQLTable):
     def __init__(self, client, token):
         GraphQLTable.__init__(self, client, token)
         self.record_name = "pipelineimage"
@@ -74,15 +54,29 @@ class Pipelineimages(GraphQLTable):
         graphql_query = graphql_query_factory(self.table_name, self.record_name, id, filters)
         return GraphQLTable.list_graphql(self, graphql_query)
 
-    def create(self, image, image_type, rank, processing_id):
-        prepared_image = prepare_image(image)
-        self.create_variables = {
-            "image": prepared_image,
-            "image_type": image_type,
-            "rank": rank,
-            "processing_id": processing_id,
-        }
-        return self.create_graphql()
+    def create(
+            self,
+            pipeline_run_id,
+            image_path,
+            image_type,
+            resolution,
+            cleaned,
+        ):
+        # Open the file in binary mode
+        with open(image_path, 'rb') as file:
+            variables = {
+                "pipeline_run_id": pipeline_run_id,
+                "image_type": image_type,
+                "resolution": resolution,
+                "cleaned": cleaned,
+            }
+            files = {
+                "image_upload": file,
+            }
+            # Post to the rest api
+            response = requests.post(f'{self.client.rest_api_url}image/', data=variables, files=files)
+
+        return response
 
     def update(self, id, image, image_type, rank, processing_id):
         prepared_image = prepare_image(image)
@@ -99,7 +93,13 @@ class Pipelineimages(GraphQLTable):
         """Parse the arguments collected by the CLI."""
         self.print_stdout = True
         if args.subcommand == "create":
-            return self.create(args.image, args.image_type, args.rank, args.processing_id)
+            return self.create(
+                args.pipeline_run_id,
+                args.image_path,
+                args.image_type,
+                args.resolution,
+                args.cleaned,
+            )
         elif args.subcommand == "update":
             return self.update(args.id, args.image, args.image_type, args.rank, args.processing_id)
         elif args.subcommand == "list":
@@ -111,7 +111,7 @@ class Pipelineimages(GraphQLTable):
 
     @classmethod
     def get_name(cls):
-        return "pipelineimages"
+        return "pipelineimage"
 
     @classmethod
     def get_description(cls):
@@ -132,30 +132,38 @@ class Pipelineimages(GraphQLTable):
         subs.required = True
 
         # create the parser for the "list" command
-        parser_list = subs.add_parser("list", help="list existing Pipelineimages")
-        parser_list.add_argument("--id", metavar="ID", type=int, help="list Pipelineimages matching the id [int]")
+        parser_list = subs.add_parser("list", help="list existing PipelineImage")
+        parser_list.add_argument("--id", metavar="ID", type=int, help="list PipelineImage matching the id [int]")
         parser_list.add_argument(
-            "--processing_id", metavar="PROC", type=int, help="list Pipelineimages matching the processing id [int]"
+            "--processing_id", metavar="PROC", type=int, help="list PipelineImage matching the processing id [int]"
         )
 
         # create the parser for the "create" command
-        parser_create = subs.add_parser("create", help="create a new pipelineimage")
+        parser_create = subs.add_parser("create", help="Create a new PipelineImage")
+        parser_create.add_argument(
+            "pipeline_run_id", metavar="RUN", type=int, help="ID of the related pipeline run [int]"
+        )
+        parser_create.add_argument(
+            "image_path", metavar="IMG", type=str, help="Path to the image to be uploaded [str]"
+        )
         parser_create.add_argument(
             "image_type",
             metavar="TYPE",
             type=str,
-            help='description of image type, e.g., "flux" or "snr" or "bandpass" [str]',
+            help='Description of image type from ("profile", "profile-pol", "phase-time", "phase-freq", "bandpass", "snr-cumul", "snr-single") [str]',
         )
         parser_create.add_argument(
-            "processing_id", metavar="PROC", type=int, help="id of the related processing [int]"
+            "resolution",
+            metavar="RES",
+            type=str,
+            help='Resolution of the image from ("high", "low") [str]',
         )
         parser_create.add_argument(
-            "rank",
-            metavar="RANK",
-            type=int,
-            help="rank of the image, used to indicate the order of displaing the image [int]",
+            "--cleaned",
+            action="store_true",
+            default=False,
+            help='If the image is from cleaned data (RFI removed) [bool] (default: False)',
         )
-        parser_create.add_argument("image", metavar="IMG", type=str, help="path to the image to be uploaded [str]")
 
         # create the parser for the "update" command
         parser_update = subs.add_parser("update", help="update a new pipelineimage")
@@ -183,12 +191,12 @@ class Pipelineimages(GraphQLTable):
 
 
 if __name__ == "__main__":
-    parser = Pipelineimages.get_parsers()
+    parser = PipelineImage.get_parsers()
     args = parser.parse_args()
 
     from psrdb.graphql_client import GraphQLClient
 
     client = GraphQLClient(args.url, args.very_verbose)
 
-    p = Pipelineimages(client, args.url, args.token)
+    p = PipelineImage(client, args.url, args.token)
     p.process(args)
