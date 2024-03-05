8e29b5ee433acb706e51a1d2a39d3a134cd8973c
NickSwainston <nickaswainston@gmail.com>
2024-03-05 12:28:55 +1100

    Uncomment part of the dev script

--- dev_scripts/generate_all_results_jsons_molonglo.sh
+++ dev_scripts/generate_all_results_jsons_molonglo.sh
@@ -4,25 +4,25 @@ set -e
 
 
 # Upload all results, images and toas
-# for path in $(find /fred/oz002/ldunn/meertime_dataportal/data/post/ -type f -name "obs.header"); do
-#     echo "PATH: $PATH"
-#     IFS="/"
-#     # Split the path into an array
-#     read -ra directories <<< "$path"
-#     echo "directories: $directories"
-#     utc=${directories[-2]}
-#     jname=${directories[-3]}
-#     echo "janme utc: $jname $utc"
-#     if [ -f "/fred/oz002/ldunn/meertime_dataportal/data/post/${jname}/${utc}/results.json" ]; then
-#         python ingest_molonglo_results.py --pulsar ${jname} --date ${utc}  && returncode=$? || returncode=$?
-#         if [ $returncode -eq 0 ]; then
-#             echo "Results uploaded"
-#         else
-#             echo "Trying again in 30 seconds"
-#             sleep 30
-#         fi
-#     fi
-# done
+for path in $(find /fred/oz002/ldunn/meertime_dataportal/data/post/ -type f -name "obs.header"); do
+    echo "PATH: $PATH"
+    IFS="/"
+    # Split the path into an array
+    read -ra directories <<< "$path"
+    echo "directories: $directories"
+    utc=${directories[-2]}
+    jname=${directories[-3]}
+    echo "janme utc: $jname $utc"
+    if [ -f "/fred/oz002/ldunn/meertime_dataportal/data/post/${jname}/${utc}/results.json" ]; then
+        python ingest_molonglo_results.py --pulsar ${jname} --date ${utc}  && returncode=$? || returncode=$?
+        if [ $returncode -eq 0 ]; then
+            echo "Results uploaded"
+        else
+            echo "Trying again in 30 seconds"
+            sleep 30
+        fi
+    fi
+done
 
 # Download those ToAs and create residuals and upload them
 
